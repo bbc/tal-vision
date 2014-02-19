7a27d606f5840168b1eedd39507b14576d9349e4
Thomas Parisot <thomas.parisot@bbc.co.uk>
2014-02-19 12:18:32 +0000

    Home container

--- static/js/appui/components/home.js
+++ static/js/appui/components/home.js
@@ -2,25 +2,49 @@ require.def('lancaster-vision/appui/components/home',
   [
     "antie/widgets/component",
     "antie/widgets/button",
-    "antie/widgets/label"
+    "antie/widgets/label",
+    "antie/widgets/componentcontainer",
+    "antie/widgets/verticallist",
+    "antie/widgets/horizontallist"
   ],
-  function(Component, Button, Label) {
+  function(Component, Button, Label, ComponentContainer, VerticalList, HorizontalList) {
+
+    var createLabelledButton = function (label, id){
+      var button = new Button(id);
+      var label = new Label(label);
+
+      button.appendChildWidget(label);
+
+      return button;
+    };
 
     return Component.extend({
       init: function () {
-        var self, label, button;
+        var self = this;
+        var widgets = [];
 
-        self = this;
-        // It is important to call the constructor of the superclass
         this._super("home");
 
-        // Hello World
-        label = new Label("Hello World");
-        this._button = new Button();
-        this._button.appendChildWidget(label);
+        // Vertical Skeleton Navigation
+        var skeleton = new VerticalList('content-to-navigation');
 
-        this.addEventListener("beforerender", function (ev) {
-          self._onBeforeRender(ev);
+        // Horizontal Menu
+        var menu = new HorizontalList();
+        menu.appendChildWidget(createLabelledButton('Home'));
+        menu.appendChildWidget(createLabelledButton('Browse'));
+        menu.appendChildWidget(createLabelledButton('History'));
+        menu.appendChildWidget(createLabelledButton('Search'));
+        menu.appendChildWidget(createLabelledButton('Trending'));
+
+        // Ordering Elements
+        skeleton.appendChildWidget(new ComponentContainer('content-container'));
+        skeleton.appendChildWidget(menu);
+
+        //
+        widgets.push(skeleton);
+
+        this.addEventListener("beforerender", function (e) {
+          self._onBeforeRender(e, widgets);
         });
 
         // calls Application.ready() the first time the component is shown
@@ -33,8 +57,12 @@ require.def('lancaster-vision/appui/components/home',
 
       // Appending widgets on beforerender ensures they're still displayed
       // if the component is hidden and subsequently reinstated.
-      _onBeforeRender: function () {
-        this.appendChildWidget(this._button);
+      _onBeforeRender: function (e, widgets) {
+        var self = this;
+
+        widgets.forEach(function(widget){
+          self.appendChildWidget(widget);
+        });
       }
     });
   }
