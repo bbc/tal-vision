6302091897f406e09f5e87cdd7c84323350e4a18
Anthony Onumonu <cisnky@gmail.com>
2014-02-19 17:59:21 +0000

    Initial VOD

--- static/js/appui/components/home.js
+++ static/js/appui/components/home.js
@@ -1,23 +1,64 @@
 require.def('lancaster-vision/appui/components/home',
   [
+    "antie/widgets/componentcontainer",
     "antie/widgets/component",
     "antie/widgets/button",
-    "antie/widgets/label"
+    "antie/widgets/label",
+    "antie/widgets/image",
+    "antie/mediasource",
+    "antie/widgets/horizontallist"
   ],
-  function(Component, Button, Label) {
+  function(Container, Component, Button, Label, Image, MediaSource, HorizontalList) {
 
     return Component.extend({
       init: function () {
-        var self, label, button;
+        var app, self, label, button, image, device, playLabel, pauseLabel;
 
         self = this;
+        app = this.getCurrentApplication();
+        device = app.getDevice();
+
         // It is important to call the constructor of the superclass
         this._super("home");
 
-        // Hello World
-        label = new Label("Hello World");
-        this._button = new Button();
-        this._button.appendChildWidget(label);
+        this._controls = new HorizontalList('random');
+
+        playLabel = new Label("Play");
+        this._play = new Button();
+        this._play.appendChildWidget(playLabel);
+        this._play.addEventListener('select', function(e){
+          self._videoPlayer.play();
+        });
+
+        this._controls.appendChildWidget(this._play);
+
+        pauseLabel = new Label("Pause");
+        this._pause = new Button();
+        this._pause.appendChildWidget(pauseLabel);
+        this._pause.addEventListener('select', function(e){
+          self._videoPlayer.pause();
+        });
+
+        this._controls.appendChildWidget(this._pause);
+
+        this._videoPlayer = device.createPlayer("player", "video");
+
+        function queueVideo(id)
+        {
+          self._videoPlayer.setSources([
+            new MediaSource("http://148.88.67.137/"+id+".mp4", "video/mp4")
+          ]);
+          self._videoPlayer.load();
+        }
+
+         device.loadURL("http://localhost:3000/api_stubs/viewing_history.json", {
+          onLoad: function(responseObject) {
+            var data = JSON.parse(responseObject);
+            queueVideo(data[0].programme_id);
+          },
+          onError: function(response) {
+          }
+        });
 
         this.addEventListener("beforerender", function (ev) {
           self._onBeforeRender(ev);
@@ -34,7 +75,9 @@ require.def('lancaster-vision/appui/components/home',
       // Appending widgets on beforerender ensures they're still displayed
       // if the component is hidden and subsequently reinstated.
       _onBeforeRender: function () {
-        this.appendChildWidget(this._button);
+        this.appendChildWidget(this._controls);
+        this.appendChildWidget(this._videoPlayer);
+
       }
     });
   }
