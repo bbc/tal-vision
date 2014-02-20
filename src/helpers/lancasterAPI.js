37e6ecb7d7a63b512549c636df527b1d0d6ee9ca
Thomas Parisot <thomas.parisot@bbc.co.uk>
2014-02-20 10:59:15 +0000

    Removing the API key check.

--- src/helpers/lancasterAPI.js
+++ src/helpers/lancasterAPI.js
@@ -4,7 +4,8 @@ module.exports = function(app){
   return function (){
     // Checking Application Key
     if (!process.env.APPLICATION_KEY){
-      throw new Error("Lancaster Application Key is missing. Please refer to README.md to configure it.");
+      console.log("Lancaster Application Key is missing. Update lancasterAPI.js when ready to deploy in production.");
+      //throw new Error("Lancaster Application Key is missing. Please refer to README.md to configure it.");
     }
 
     app.locals.APPLICATION_KEY = process.env.APPLICATION_KEY;
