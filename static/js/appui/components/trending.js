405eccee63912b5da4578190113d714bea649141
Ross Wilson <me@rosswilson.co.uk>
2014-02-20 12:01:18 +0000

    Trending carousel items link to dummy Home component, renamed trending formatter

--- static/js/appui/components/trending.js
+++ static/js/appui/components/trending.js
@@ -1,16 +1,17 @@
 require.def('lancaster-vision/appui/components/trending',
   [
+    "antie/application",
     "antie/widgets/component",
     "antie/datasource",
     "antie/widgets/carousel",
     "antie/widgets/label",
-    "lancaster-vision/appui/formatters/simpleformatter",
+    "lancaster-vision/appui/formatters/trendingformatter",
     "lancaster-vision/appui/datasources/trendingfeed",
     "antie/widgets/carousel/binder",
     "antie/widgets/carousel/keyhandlers/alignfirsthandler",
     "antie/widgets/button"
   ],
-  function (Component, DataSource, Carousel, Label, SimpleFormatter, TrendingFeed, Binder, AlignFirstHandler, Button) {
+  function (Application, Component, DataSource, Carousel, Label, TrendingFormatter, TrendingFeed, Binder, AlignFirstHandler, Button) {
 
     // All components extend Component
     return Component.extend({
@@ -19,7 +20,7 @@ require.def('lancaster-vision/appui/components/trending',
         this._super("trending_carousel_component");
 
         // Create a simple formatter and data feed that will be used to populate the carousel
-        var trendingFormatter = new SimpleFormatter();
+        var trendingFormatter = new TrendingFormatter();
         var trendingFeed = new TrendingFeed(this);
         this._dataSource = new DataSource(this, trendingFeed, "loadData");
 
@@ -39,7 +40,6 @@ require.def('lancaster-vision/appui/components/trending',
         var keyhandler = new AlignFirstHandler();
         keyhandler.attach(this._carousel);
 
-
         this.addEventListener("beforerender", function (ev) {
           self._onBeforeRender(ev);
         });
@@ -48,6 +48,15 @@ require.def('lancaster-vision/appui/components/trending',
       _onDataBound: function (evt) {
         var children = this._carousel.getChildWidgets();
         children[0].focus();
+
+        // Launch the components
+        this._carousel.getChildWidgets().forEach(function(widget) {
+          widget.addEventListener('select', function() {
+            var root = Application.getCurrentApplication().getRootWidget();
+            console.log(root);
+            root.getChildWidget('maincontainer').getChildWidget('maincontroller').getChildWidget('content-to-navigation').getChildWidget('content-container').pushComponent('lancaster-vision/appui/components/home');
+          });
+        });
       },
 
       // Appending widgets on beforerender ensures they're still displayed
