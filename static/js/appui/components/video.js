88b507b021847b61002a75e2bd1d44e38d78ba46
Ross Wilson <me@rosswilson.co.uk>
2014-03-09 15:52:44 +0000

    Added player instance and segment/heartbeat statistics reporting to player, added listener to update scrubbar handle position as playback progresses, removed focus listener handler for navbuttons once player view isn't active

--- static/js/appui/components/video.js
+++ static/js/appui/components/video.js
@@ -30,11 +30,13 @@ require.def('lancaster-vision/appui/components/video',
         // Scrub bar
         this._scrub = new ScrubBar('scrub', 0, 0.01, 0.05, 1);        
         this._scrub.addEventListener("sliderchangeend", function () {
+            self._scrubbing = false;
             var time = self._videoPlayer.getDuration() * self._scrub.getValue();
             self._videoPlayer.setCurrentTime(time)
             self._videoPlayer.play();
         });
         this._scrub.addEventListener("sliderchange", function () {
+            self._scrubbing = true;
             self.showControls();
             self.setControlsHideTimeout();
         });
@@ -80,36 +82,70 @@ require.def('lancaster-vision/appui/components/video',
         });
 
         this.addEventListener("beforehide", function(ev) {
-          self.clearControlsHideTimeout();
           self.showHeader();
           self.showControls();
+          self.clearControlsHideTimeout();
+          self._app.getRootWidget().getChildWidget("maincontainer").getChildWidget("maincontroller").getMenu().removeEventListener("focus", self._focusNavBar);        
         });
 
         this.addEventListener("focus", function(ev) {
           self.showControls();
           self.setControlsHideTimeout();
+
         });
 
-        self._app.getRootWidget().getChildWidget("maincontainer").getChildWidget("maincontroller").getMenu().addEventListener("focus", function(ev) {
+        this._focusNavBar = function(ev) {
           self.showControls();
           self.setControlsHideTimeout();
-        });
+        };
 
-        this._videoPlayer.addEventListener("progress", function () {
+        self._app.getRootWidget().getChildWidget("maincontainer").getChildWidget("maincontroller").getMenu().addEventListener("focus", self._focusNavBar);
+
+        this._videoPlayer.addEventListener("timeupdate", function () {
           var player = self._videoPlayer;
-
-          var buffer = player.getBuffered();
           var current_pos = player.getCurrentTime();
 
+          // Set scrubbar current position indicator
+          if(!self._scrubbing) {
+            self._scrub.setValue(current_pos / player.getDuration());
+          }
+
+          // Set scrubbar buffered media area
+          var buffer = player.getBuffered();
           self._scrub.setBufferedRange({
             start: 0,
             end: buffer.end(buffer.length - 1) / player.getDuration()
           });
+
+          var currentSeconds = Math.floor(player.getCurrentTime());
+
+          // Only true once per second
+          if(currentSeconds != this._last_segment_second) {
+            if(currentSeconds % 5 == 0) {
+
+              if(currentSeconds < this._last_segment_second || currentSeconds > this._last_segment_second + 5) {
+                self._last_segment_start = currentSeconds;
+                console.log("Logging skipped segment %d:%d", self._last_segment_start, currentSeconds);
+              } else {
+                console.log("Logging normal segment %d:%d", self._last_segment_start, currentSeconds);
+              }
+
+              self.logPlayerSegment(self._last_segment_start, currentSeconds);
+
+              this._last_segment_second = currentSeconds;
+            }
+          }
+          
         });
 
         // calls Application.ready() the first time the component is shown
         // the callback removes itself once it's fired to avoid multiple calls.
         this.addEventListener("aftershow", function appReady() {
+          document.getElementById("player").addEventListener("loadedmetadata", function() {
+            console.log("Setting start time to %d", self._start_time);
+            self._videoPlayer.setCurrentTime(self._start_time);
+          });
+
           self.getCurrentApplication().ready();
           self.removeEventListener('aftershow', appReady);
         });
@@ -146,30 +182,70 @@ require.def('lancaster-vision/appui/components/video',
       },
 
       clearControlsHideTimeout: function() {
-        var self = this;
-        clearTimeout(self._timeout);
+        clearTimeout(this._timeout);
       },
 
       // Appending widgets on beforerender ensures they're still displayed
       // if the component is hidden and subsequently reinstated.
       _onBeforeRender: function (e) {
-        var self = this;
-
         this.hideHeader();
 
         this.appendChildWidget(this._videoPlayer);
         this.appendChildWidget(this._gui_list);
-        this.queueVideo(e.args.programme_id);
 
-        self.setControlsHideTimeout();
+        this.setControlsHideTimeout();
+
+        this.logPlayerInstance(e.args);
+      
+        this.queueVideo(e.args.programme_id);
       },
 
-      queueVideo: function(id) {
+      queueVideo: function(id, startTime) {
+        this._start_time = startTime || 60;
+
+        this._last_segment_start = this._start_time;
+        this._last_segment_second = this._start_time;
+
         this._videoPlayer.setSources([
           new MediaSource("http://148.88.32.70/" + id + ".mp4", "video/mp4")
         ]);
         this._videoPlayer.load();
         this._videoPlayer.play();
+      },
+
+      logPlayerInstance: function(programme) {
+        this._heartbeat_id = CryptoJS.SHA1("tal" + Math.random() + programme.programme_id).toString();
+
+        console.log("Logging player instance, heartbeat ID: %s", this._heartbeat_id);
+
+        var file_url = "http://148.88.32.70/" + programme.programme_id + ".mp4";
+
+        $.ajax({
+          url: "http://10.42.32.75:9110/capture/player_instance",
+          type: "get",
+          data: {
+            api: "53e659a15aff4a402de2d51b98703fa1ade5b8c5",
+            heartbeat_id: this._heartbeat_id,
+            user_id: 202,
+            programme_id: programme.programme_id,
+            file_id: file_url
+          }
+        });
+
+      },
+
+      logPlayerSegment: function(start, end) {
+        $.ajax({
+          url: "http://10.42.32.75:9110/capture/heartbeat",
+          type: "get",
+          data: {
+            api: "53e659a15aff4a402de2d51b98703fa1ade5b8c5",
+            heartbeat_id: this._heartbeat_id,
+            user_id: 202,
+            start: start,
+            end: end
+          }
+        });
       }
     });
   }
