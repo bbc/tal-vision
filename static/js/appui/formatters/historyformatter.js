5e86029c96972d05be207249361d0aa19ab8b880
Ross Wilson <me@rosswilson.co.uk>
2014-03-10 11:20:30 +0000

    New History screen, with pause resume functionality working

--- static/js/appui/formatters/historyformatter.js
+++ static/js/appui/formatters/historyformatter.js
@@ -13,15 +13,15 @@ require.def('lancaster-vision/appui/formatters/historyformatter',
         var button, item, list;
         item = iterator.next();
 
-        var list = new HorizontalList("history_" + item.programme_id);
+        var list = new VerticalList("history_" + item.programme_id);
 
-        var width = 302;
-        var height = 165;
+        var width = 350;
+        var height = 205;
         item.image_url = "http://148.88.32.64/cache/" + width + "x" + height + "-2/programmes/" + item.image;
 
         list.appendChildWidget(new Image("trending_img_" + item.programme_id, item.image_url, { width : width, height: height}));
 
-        var prog_details_list = new VerticalList();
+        var prog_details_list = new VerticalList("prog_details");
         prog_details_list.addClass("prog_details");
         var prog_name_label = new Label(item.programme_name);
         prog_name_label.addClass("prog_name");
@@ -32,19 +32,25 @@ require.def('lancaster-vision/appui/formatters/historyformatter',
         prog_details_list.appendChildWidget(synopsis);
         list.appendChildWidget(prog_details_list);
 
-        var buttons_list = new HorizontalList();
+        var buttons_list = new VerticalList("buttons_list");
 
-        var play = new Button();
-        play.appendChildWidget(new Label("Play From Begining"));
-        play.addClass("play_button");
-        buttons_list.appendChildWidget(play);
+        this._play = new Button("play_button");
+        this._play.appendChildWidget(new Label("Play From Begining"));
+        this._play.addClass("play_button");
+        buttons_list.appendChildWidget(this._play);
 
-        var resume = new Button();
-        resume.appendChildWidget(new Label("Resume"));
-        resume.addClass("resume_button");
-        buttons_list.appendChildWidget(resume);
+        this._resume = new Button("resume_button");
+        this._resume.appendChildWidget(new Label("Resume"));
+        this._resume.addClass("resume_button");
+        buttons_list.appendChildWidget(this._resume);
 
         prog_details_list.appendChildWidget(buttons_list);
+
+        // Check the VOD is available
+        if(item.vod_status != "COMPLETE") {
+          this._play.setDisabled(true);
+          this._resume.setDisabled(true);
+        }
 
         list.setDataItem(item);
         return list;
